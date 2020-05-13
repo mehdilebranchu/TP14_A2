21750047f5456fdc3fa06de8b203c412cdba1191
lebranchu mehdi <mehdi.lebranchu@gmail.com>
2020-05-13 17:20:58 +0200

    ex3 finis

--- ex3.py
+++ ex3.py
@@ -1,21 +1,25 @@
 from PySide2.QtWidgets import *
+from PySide2.QtGui import *
 
 class Window(QWidget):
     def __init__(self):
         QWidget.__init__(self)
         self.setWindowTitle("IHM")
         self.setFixedSize(400,300)
-        self.__icon = (C:\Users\lebranchu mehdi\Downloads\drapeau.png,r)
-        self.setWindowIcon(self.__icon)
-        self.__layout = QGridLayout()
+        self.myicon = QIcon("drapeau.png")
+        self.setWindowIcon(self.myicon)
+        self.__layout = QVBoxLayout()
         self.__label = QLabel("laisser un commentaire")
-        self.__button = QPushButton("success")
-        self.__button1 = QPushButton("cancel")
-        self.__text = QTextEdit("")
+        self.__label.setAlignment(Qt.AlignCenter)
+        self.__bar = QProgressBar()
+        self.__bar.setValue(50)
+        self.__line = QLineEdit()
+        self.__button = QPushButton('Click me')
+        self.__button.setToolTip('veuillez patienter')
         self.__layout.addWidget(self.__label)
-        self.__layout.addWidget(self.__text)
+        self.__layout.addWidget(self.__bar)
+        self.__layout.addWidget(self.__line)
         self.__layout.addWidget(self.__button)
-        self.__layout.addWidget(self.__button1)
         self.setLayout(self.__layout)
 
 
