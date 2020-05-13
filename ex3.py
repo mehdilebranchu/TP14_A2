1e16f6fa847fd2a5e76ae95d4eb9a8a96acd737a
lebranchu mehdi <mehdi.lebranchu@gmail.com>
2020-05-13 17:03:29 +0200

    J'ai des pb avec le chemin du fichier

---
+++ ex3.py
@@ -0,0 +1,26 @@
+from PySide2.QtWidgets import *
+
+class Window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setWindowTitle("IHM")
+        self.setFixedSize(400,300)
+        self.__icon = (C:\Users\lebranchu mehdi\Downloads\drapeau.png,r)
+        self.setWindowIcon(self.__icon)
+        self.__layout = QGridLayout()
+        self.__label = QLabel("laisser un commentaire")
+        self.__button = QPushButton("success")
+        self.__button1 = QPushButton("cancel")
+        self.__text = QTextEdit("")
+        self.__layout.addWidget(self.__label)
+        self.__layout.addWidget(self.__text)
+        self.__layout.addWidget(self.__button)
+        self.__layout.addWidget(self.__button1)
+        self.setLayout(self.__layout)
+
+
+if __name__ == "__main__":
+   app = QApplication([])
+   win = Window()
+   win.show()
+   app.exec_()
